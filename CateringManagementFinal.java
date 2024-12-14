663dffd91d3ec54a52e61f8c402f1deea3105e09
Davelimboc6 <davelimboc6@gmail.com>
2024-12-14 20:34:37 +0800

    Add files via upload
    Final Output

---
+++ CateringManagementFinal.java
@@ -0,0 +1,13 @@
+package catering.management.pkgfinal;
+
+public class CateringManagementFinal {
+
+    public static void main(String[] args) {
+        
+        LogIn LogInFrame = new LogIn();
+        LogInFrame.setVisible(true);
+        LogInFrame.pack();
+        LogInFrame.setLocationRelativeTo(null);
+    }
+   
+}
